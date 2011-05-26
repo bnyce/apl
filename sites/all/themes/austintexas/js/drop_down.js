3f982d6e24d8ffde5f727d1b5fb31f5e2fe01788
Alex Rodriguez <alex.rodriguez@ci.austin.tx.us>
2011-05-26 17:41:25 +0000

    changed background color for banner links

--- sites/all/themes/austintexas/js/drop_down.js
+++ sites/all/themes/austintexas/js/drop_down.js
@@ -105,7 +105,34 @@
 		}
 		
 		
+
+
+		var secondary_menu_id = $('#submenubar .content ul').attr('id');
+
+		switch(secondary_menu_id)
+		{
 		
+			case 'nice-menu-1':
+			$('.rb-textbox-wrapper').css('background', '#767f89');
+			break;
+			
+			case 'nice-menu-2':
+			$('#main-menu-links .menu-572').attr('class', 'menu-572 active');
+			break;
+
+			case 'nice-menu-3':
+			$('#main-menu-links .menu-573').attr('class', 'active');
+			break;
+
+			case 'nice-menu-4':
+			$('#main-menu-links .menu-574').attr('class', 'active');
+			break;
+
+			case 'nice-menu-5':
+			$('#main-menu-links .menu-575').attr('class', 'active');
+			break;
+		
+		}
 		
 		
 		   

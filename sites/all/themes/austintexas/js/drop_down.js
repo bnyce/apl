79844e4022be881fc20669c3922538b2bd65b7e1
Alex Rodriguez <alex.rodriguez@ci.austin.tx.us>
2011-05-26 13:39:09 +0000

    added switch to create active class for respective main menu item

--- sites/all/themes/austintexas/js/drop_down.js
+++ sites/all/themes/austintexas/js/drop_down.js
@@ -68,6 +68,46 @@
                 if (! $clicked.parents().hasClass("dropdown3"))
                     $(".dropdown3 dd ul").hide();
             });
+		
+		
+		
+		
+		
+		//set active class for main menu items
+		
+		// get id of secondary menu
+		var secondary_menu_id = $('#submenubar .content ul').attr('id');
+		//alert(secondary_menu_id);
+		
+		switch(secondary_menu_id)
+		{
+		
+			case 'nice-menu-1':
+			$('#main-menu-links .menu-877').attr('class', 'active');
+			break;
+			
+			case 'nice-menu-2':
+			$('#main-menu-links .menu-572').attr('class', 'active');
+			break;
+
+			case 'nice-menu-3':
+			$('#main-menu-links .menu-573').attr('class', 'active');
+			break;
+
+			case 'nice-menu-4':
+			$('#main-menu-links .menu-574').attr('class', 'active');
+			break;
+
+			case 'nice-menu-5':
+			$('#main-menu-links .menu-575').attr('class', 'active');
+			break;
+		
+		}
+		
+		
+		
+		
+		
 		   
         });
 		

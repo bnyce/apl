f0ab3e14b34790498c0a6ba5828e392a8396aa65
Aaron Wagner <aaron.wagner@ci.austin.tx.us>
2011-06-01 14:58:58 +0000

    shift off HOME from breadcrumbs

--- sites/all/themes/austintexas/template.php
+++ sites/all/themes/austintexas/template.php
@@ -4,12 +4,15 @@ function austintexas_breadcrumb($variables) {
   if (!empty($breadcrumb)) {
     // Adding the title of the current page to the breadcrumb.
     $breadcrumb[] = drupal_get_title();
-   
+
+    //shift off the HOME crumb		    
+    array_shift($breadcrumb);
+
     // Provide a navigational heading to give context for breadcrumb links to
     // screen-reader users. Make the heading invisible with .element-invisible.
     $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
 
-    $output .= '<div class="breadcrumb">' . implode(' » ', $breadcrumb) . '</div>';
+    $output .= '<div class="breadcrumb">' . implode(' » ', $breadcrumb) . '</div>'; 
     return $output;
   }
 }

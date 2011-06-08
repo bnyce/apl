e2251b49ad74f10dc40b738814135e3b4c440504
Alex Rodriguez <alex.rodriguez@ci.austin.tx.us>
2011-06-08 14:49:55 +0000

    added select custom style

--- sites/all/themes/austintexas/js/drop_down.js
+++ sites/all/themes/austintexas/js/drop_down.js
@@ -70,9 +70,49 @@
             });
 		
 		
+
+
+
 		
+		//set active class for main menu items
 		
+		// get id of secondary menu
+		var secondary_menu_id = $('#submenubar .content ul').attr('id');
+		//alert(secondary_menu_id);
 		
+		switch(secondary_menu_id)
+		{
+		
+			case 'nice-menu-1':
+			$('#main-menu-links .menu-877').attr('class', 'active');
+			break;
+			
+			case 'nice-menu-2':
+			$('#main-menu-links .menu-572').attr('class', 'menu-572 active');
+			break;
+
+			case 'nice-menu-3':
+			$('#main-menu-links .menu-573').attr('class', 'active');
+			break;
+
+			case 'nice-menu-4':
+			$('#main-menu-links .menu-574').attr('class', 'active');
+			break;
+
+			case 'nice-menu-5':
+			$('#main-menu-links .menu-575').attr('class', 'active');
+			break;
+		
+		}
+
+
+
+
+
+
+	//online services select lists
+	$('select.form-select').customStyle();
+
 		   
         });
 		

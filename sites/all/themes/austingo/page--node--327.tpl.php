e927180ffd786489b0fabd59bfa06cc12139f1e3
Alex Rodriguez <alex.rodriguez@ci.austin.tx.us>
2011-06-15 18:02:21 +0000

    327 - removed rotating banner

--- sites/all/themes/austingo/page--node--327.tpl.php
+++ sites/all/themes/austingo/page--node--327.tpl.php
@@ -51,8 +51,12 @@
       </div>
     <?php endif; ?>
         <div id="submenubar"><?php print render($page['submenubar']); ?></div>
-		<div id="header-banner" class="br"><?php print render($page['header_banner']); ?></div>
+        
+        
+		<!-- <div id="header-banner" class="br"><?php print render($page['header_banner']); ?></div> -->
         <!-- <div id="search" class="br"><?php print render($page['search']); ?></div> -->
+        
+        
       </div> <!-- /#header -->
 
       <div id="center"><div id="squeeze"><div class="right-corner"><div class="left-corner">

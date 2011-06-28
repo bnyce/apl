482331ff0b141305c0d58bade79ffc224c82577e
awagner <aaron.wagner@ci.austin.tx.us>
2011-06-28 11:56:09 -0500

    ghh

--- sites/all/modules/fullcalendar/js/fullcalendar.views.js
+++ sites/all/modules/fullcalendar/js/fullcalendar.views.js
@@ -131,6 +131,23 @@ Drupal.behaviors.fullCalendar = {
       return false;
     });
 
+    $('.fc-view-basicWeek .fc-widget-content, a.calendarEventDay', context).click(function () {
+      // This function will get exceuted after the ajax request is completed successfully
+      var updateProducts = function(data) {
+        // The data parameter is a JSON object. The “products” property is the list of products items that was returned from the server response to the ajax request.
+        alert('here');
+        //$('#view-display-id-calendar_day_block').html(data.products);
+      }
+      $.ajax({
+        type: 'POST',
+        url: 'xxxfullcalendar/update/20', //this.href, // Which url should be handle the ajax request. This is the url defined in the <a> html tag
+        success: updateProducts, // The js function that will be called upon success request
+        dataType: 'json', //define the type of data that is going to get back from the server
+        data: 'js=1' //Pass a key/value pair
+      });
+      return false;  // return false so the navigation stops here and not continue to the page in the link
+    });
+
     // Trigger a window resize so that calendar will redraw itself as it loads funny in some browsers occasionally
     $(window).resize();
   }

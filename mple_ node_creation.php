491d228382890e5d2d4999863ee0b433c186ea02
Srilakshmi Nandigam <srilakshmi.nandigam@ci.austin.tx.us>
2011-07-25 17:03:41 -0500

    added timestamp comparision logic to mple_ node_creation.php

--- mple_ node_creation.php
+++ mple_ node_creation.php
@@ -1,25 +1,70 @@
 <?php
-	if ($handle = opendir('C:/Documents and Settings/nandigamS/Desktop/htmlfiles')) {
-		    while (false !== ($file = readdir($handle))) {
+    $path="C:/Documents and Settings/nandigamS/My Documents/cityclerck/";
+    if($handle=opendir($path)){
+        $time_stamp="";
+         while (false !== ($file0 = readdir($handle))) {
+            $recent_dir="";
+            if ($file0 != "." && $file0 != "..") {   //    strip out . and .. 
+                echo $file0."<br/>";
+				$file_name=explode("_",$file0);
+                $seconds=substr($file_name[1],0,2);
+                $minuts=substr($file_name[1],2,2);
+                $hours=substr($file_name[1],4,2);
+                $file_name[1]=$hours.$minuts.$seconds;
+                $time_stamp[]=strtotime($file_name[0].$file_name[1]);
+             }
+        }
+		$recent_dir= max($time_stamp);
+        $recent_dir_date=date('Ymd_siH',$recent_dir);
+        $new_path=$path.$recent_dir_date;
+		echo $new_path."<br/>";
+        if($handle1=opendir($new_path)){
+			while (false !== ($file1 = readdir($handle1))) {
+              if ($file1 != "." && $file1 != "..") {   //    strip out . and .. 
+			  	 echo $file1."<br/>";
+                  $new_path1=$new_path.'/'.$file1;
+				  echo $new_path1."<br/>";
+                  if($handle2=opendir($new_path1)){
+                      while (false !== ($file2 = readdir($handle2))) {
+						  if ($file2 != "." && $file2 != "..") {   //    strip out . and .. 
+                                echo $file2."<br/>";
+								$new_path2=$new_path1.'/'.$file2;
+								if($file2=="meetings"){
+									if($handle3=opendir($new_path2)){	
+			
+			
+			
+			
+			
+	//if ($handle = opendir('C:/Documents and Settings/nandigamS/Desktop/htmlfiles')) {
+		    while (false !== ($file = readdir($handle3))) {
 				$buffer1="";
 				if ($file != "." && $file != "..") {   //    strip out . and ..
-					$buffer1=file_get_contents("C:/Documents and Settings/nandigamS/Desktop/htmlfiles/$file");        
+				    echo $file."<br/>";
+					$new_path3=$new_path2.'/'.$file;
+					//$buffer1=file_get_contents("C:/Documents and Settings/nandigamS/Desktop/htmlfiles/$file");
+					$buffer1=file_get_contents($new_path3);        
 					if(strstr($file,".")){
 						$file_name="";
     					$offset=0;
     					$a=strpos($file,".");
     					$file_name= substr($file,$offset,$a);
+						//echo  $file_name."<br/>";
 					}
 					mysql_connect('localhost','root','root')or die("Could not connect: " . mysql_error());
                     mysql_select_db('drupaldb');
 					$result = mysql_query("select title,nid,created from scratch_node where title='$file_name'");
                 	$row = mysql_fetch_array($result);
                     $Title= $row['title'];
+					echo $Title."<br/>";
+					
 					//*********************************Node_Update******************************
 					if($Title==$file_name){ // Comparing filename with existing node name
 						echo "Node Found<br/>";
-						$file_timestamp=filemtime("C:/Documents and Settings/nandigamS/Desktop/htmlfiles/$file");
+						//$file_timestamp=filemtime("C:/Documents and Settings/nandigamS/Desktop/htmlfiles/$file");
+						$file_timestamp=filemtime($new_path3);
 						echo $file_timestamp."Timestamp<br/>";
+						echo $row['created']."created time<br/>";
 						if($row['created']==$file_timestamp){
 							echo "There is no change in timestamp,So no need to update<br/>";
 						}else{
@@ -38,6 +83,7 @@
 								echo "Node with nid " . $node->nid . " updated!<br/>";
 							}
 						}
+						
 					//*********************************New_Node_Creation******************************	
 					}elseif(!($Title==$file_name)){
 						echo "Node not Found<br/>New node created<br/>";
@@ -45,7 +91,9 @@
 						$_SERVER['REMOTE_ADDR'] = "localhost"; // Necessary if running from command line
 						require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 						drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-						$file_timestamp=filemtime("C:/Documents and Settings/nandigamS/Desktop/htmlfiles/$file");
+						//$file_timestamp=filemtime("C:/Documents and Settings/nandigamS/Desktop/htmlfiles/$file");
+						$file_timestamp=filemtime($new_path3);
+						
 						$bodytext = $buffer1; // Body part of the html file
 						$node = new stdClass(); // Create a new node object
 						$node->type = "page"; // or page, or whatever content type you like
@@ -81,4 +129,12 @@
 		@mysql_close();
 		closedir($handle);
      }
+									}
+								}
+						  }
+					  }
+				  }
+			  }
+			}
+		}
 ?>

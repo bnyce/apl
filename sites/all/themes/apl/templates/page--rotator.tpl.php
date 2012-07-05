0b36507c58ba3214266d4ea8d856b262eda67343
Bryce Benton <bryce.benton@austintexas.gov>
2012-07-05 14:07:10 -0500

    tweaks

--- sites/all/themes/apl/templates/page--rotator.tpl.php
+++ sites/all/themes/apl/templates/page--rotator.tpl.php
@@ -1,6 +1,16 @@
 <!-- slideshow -->   
 <script>
 document.documentElement.style.overflow = 'hidden';	 // firefox, chrome
+
+    window.onload = setupRefresh;
+
+    function setupRefresh() {
+      setTimeout("refreshPage();", 3600000); // milliseconds
+    }
+    function refreshPage() {
+//       alert('hi there');
+       window.location = location.href;
+    }
 </script>
 
 <div style="max-height:1080px !important; max-width:1920px !important; overflow: hidden">

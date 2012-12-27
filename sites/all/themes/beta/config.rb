3b66a558467e365f26dee55832335609ee6bcb45
Bryce Benton <bryce.benton@austintexas.gov>
2012-12-27 14:25:53 -0600

    pre-process CSS as compressed

--- sites/all/themes/beta/config.rb
+++ sites/all/themes/beta/config.rb
@@ -42,7 +42,7 @@ javascripts_dir = "js"
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 #output_style = (environment == :development) ? :expanded : :compressed
-output_style = :expanded 
+output_style = :compressed 
 
 
 # To enable relative paths to assets via compass helper functions. Since Drupal

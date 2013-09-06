46a42555a284411e490a1017df705173462187b7
Bryce Benton <bryce.benton@austintexas.gov>
2013-09-06 13:20:22 -0500

    Template for Biblio booklist displays

---
+++ sites/all/themes/apl_colores/templates/views-view-field--field-booklist.tpl.php
@@ -0,0 +1,5 @@
+<?php 
+$booklist = $row->_field_data['nid']['entity']->field_booklist['und'][0]['value'];
+?>
+<iframe title="Booklist" width="580" height="350" 
+src="http://austin.bibliocommons.com/list/list_browse/user/<?php print $booklist; ?>" frameborder="0"></iframe>

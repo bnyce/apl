2e10a1f0647170d3566b6329204f4a3afe54fc79
Bryce Benton <bryce.benton@austintexas.gov>
2013-10-23 14:10:04 -0500

    theme updates for Adminimal

--- sites/all/themes/adminimal_theme/js/meeting-rooms.js
+++ sites/all/themes/adminimal_theme/js/meeting-rooms.js
@@ -1,10 +1,10 @@
 (function ($) {
   Drupal.behaviors.room_reservation_node_form = {
     attach: function(context) {
+      $('#edit-title').val('x');
       $('.form-type-date-popup input[id*="datepicker"]', context).once('room-reservation-node-form', function() {
         $(this).change(function() {
           $('#' + $(this).attr('id').replace(/value/g, 'value2')).val($(this).val());
-//          $('#' + $(this).attr('id').replace(/value/g, 'value2')).attr('disabled', 'disabled'); //Disable
         });
       });
     }

cf22f0c1f6c0a9ea41309c04dc08a2251baa4b8f
Bryce Benton <bryce.benton@austintexas.gov>
2013-10-24 07:27:57 -0500

    meeting room enhancements to theme

--- sites/all/themes/adminimal_theme/js/meeting-rooms.js
+++ sites/all/themes/adminimal_theme/js/meeting-rooms.js
@@ -1,7 +1,7 @@
 (function ($) {
   Drupal.behaviors.room_reservation_node_form = {
     attach: function(context) {
-      $('#edit-title').val('x');
+      $('form#room-reservation-node-form #edit-title').val('x');
       $('.form-type-date-popup input[id*="datepicker"]', context).once('room-reservation-node-form', function() {
         $(this).change(function() {
           $('#' + $(this).attr('id').replace(/value/g, 'value2')).val($(this).val());

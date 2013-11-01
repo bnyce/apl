2673223895e47291e8d7aaa8ee6330e18f4cfd1b
Bryce Benton <bryce.benton@austintexas.gov>
2013-11-01 13:21:19 -0500

    Don't suppress title field

--- sites/all/themes/adminimal_theme/js/meeting-rooms.js
+++ sites/all/themes/adminimal_theme/js/meeting-rooms.js
@@ -1,7 +1,6 @@
 (function ($) {
   Drupal.behaviors.room_reservation_node_form = {
     attach: function(context) {
-      $('form#room-reservation-node-form #edit-title').val('x');
       $('.form-type-date-popup input[id*="datepicker"]', context).once('room-reservation-node-form', function() {
         $(this).change(function() {
           $('#' + $(this).attr('id').replace(/value/g, 'value2')).val($(this).val());
